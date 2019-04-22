efde355ea77ffc8ba3ae0bcd6d6984d7aff828fe
Julian Bustamante <jbustamante35@gmail.com>
2019-04-22 17:35:51 -0500

    potato updates, sync Gallium OS configs

---
+++ potato/dots/.fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/jbustamante/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/jbustamante/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/jbustamante/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/jbustamante/.fzf/shell/key-bindings.zsh"
